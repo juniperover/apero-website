1c4851457b1d630c4fb514e8ef8a5ad0eebb9c89
juniperover <j.overbeck@mbs.edu>
2019-11-08 16:53:46 +1100

    Initial file that I created in the workshop

---
+++ analysis.R
@@ -0,0 +1,13 @@
+#start with pressing top left + button and selecting R script
+#Analysis script for the project
+#Then save
+#Note that many people use Rmarkdown docs, she doesn't
+#she creates a new folder "R" in the "Files" at below right
+
+#read data
+
+#process data
+
+#graphing
+
+#print image

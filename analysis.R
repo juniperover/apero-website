b317ce22b96e6f840a2bb35c443e4b7b5ee0ba63
juniperover <j.overbeck@mbs.edu>
2019-12-16 09:49:26 +1100

    Very early files, after Jen added country and created filtering/subsetting code to select correct cases

--- analysis.R
+++ analysis.R
@@ -4,9 +4,25 @@
 #Note that many people use Rmarkdown docs, she doesn't
 #she creates a new folder "R" in the "Files" at below right
 
+library(tidyverse)
+library(janitor) 
+library(here)
+library(beepr)
+library(skimr)
+library(dplyr)
+library(IPtoCountry)
+
 #read data
+#first I imported Data_complete.csv to an R file, then:
+#write.csv(Data_complete,file="data_complete.csv")
+
+ourdata <-read.csv("data/data_complete.csv")
+d <- subset(ourdata,(country == "United States" | country == "Ireland" | country == "South Africa"
+                | country == "Canada" | country == "United Kingdom" | country == "Australia"
+                | country == "New Zealand"))
 
 #process data
+
 
 #graphing
 
---
+++ dataProcessing.R
@@ -0,0 +1,1 @@
+#start by transforming the group variable to contrast coded predictors 
